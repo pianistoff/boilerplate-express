4173d159d595df97436270e1dfc7f2cfdc7a20dd
pianistoff <severyn.h@gmail.com>
2023-05-13 15:27:43 +0300

    Send the '/views/index.html' file as a response to GET requests to the '/' path

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require("express");
 let app = express();
+const path = require("path");
+
+const filePath = path.join(__dirname, process.env.FILE_PATH);
 
 console.log("Hello World");
 
 app.get("/", (_, res) => {
-  res.send("Hello Express");
+  res.sendFile(filePath);
 });
 
 module.exports = app;
