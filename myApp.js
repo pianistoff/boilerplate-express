005a2ca8dd97362159b1c80c6d7c7a7b94da72c2
pianistoff <severyn.h@gmail.com>
2023-05-15 21:50:03 +0300

    Serve the stylesheet

--- myApp.js
+++ myApp.js
@@ -3,11 +3,14 @@ let app = express();
 const path = require("path");
 
 const filePath = path.join(__dirname, process.env.FILE_PATH);
+const publicPath = path.join(__dirname, process.env.PUBLIC_PATH);
 
 console.log("Hello World");
 
 app.get("/", (_, res) => {
   res.sendFile(filePath);
 });
+
+app.use("/public", express.static(publicPath));
 
 module.exports = app;
